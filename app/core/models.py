ea2078bf4d1f38df125b507cb91417b02a827dab
Malik Kolade <kolademalikademola@gmail.com>
2022-05-22 10:37:24 +0000

    super admin setup successfully

--- app/core/models.py
+++ app/core/models.py
@@ -7,14 +7,24 @@ from django.contrib.auth.models import (
     PermissionsMixin)
 
 
-class UserManager(BaseManager):
+class UserManager(BaseUserManager):
     # Manager for users.
 
     def create_user(self, email, password=None, **extra_fields):
+        # create and return a user
         if not email:
             raise ValueError('User must have an email address')
-        user = self.model(email=email, **extra_fields)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        # create and return a superuser
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
 
         return user
@@ -23,7 +33,7 @@ class UserManager(BaseManager):
 class User(AbstractBaseUser, PermissionsMixin):
     # users in the system
     email = models.EmailField(max_length=255, unique=True)
-    name = models.CharField(max_length=255)
+    users_name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
